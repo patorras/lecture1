78c8827f61264bf88fb727ed19cc60939612dbf4
Pedro Lourenço <pedro.lourenco@driscolls.com>
2018-12-30 15:42:23 +0000

    change

--- hello.py
+++ hello.py
@@ -1,13 +1,3 @@
-# This program says hello and asks for my nameself.
-
-print('Hello World!')
-print('What is you name?')  # ask for their name
-
-myName = input()
-print('It is good to meet you, ' + myName)
-print('The lenght of your name is: ')
-print(len(myName))
-print('What is your age?')  #ask for their age
-
+print("what is your age?") # ask for their age
 myAge = input()
-print('You will be ' + str(int(myAge) + 1) + ' in a year.')
+print("You will be " + str(int(myAge) + 1) + " in a year")
